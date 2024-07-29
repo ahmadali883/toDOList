2760c8beb433c95cb28fe1cfe1691b467f64bb61
Ahmad Ali <khahmadalikhan@gmail.com>
2024-07-29 18:00:11 +0500

    init

--- app.js
+++ app.js
@@ -135,6 +135,6 @@ app.post("/delete", async (req,res)=>{
    }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is listening on port 3000");
 });
